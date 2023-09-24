e975404851041d52647049906e779842dd02ba19
didukhroma <didukhroma@gmail.com>
2023-09-24 11:54:57 +0300

    feature: add localstorage save theme

--- src/js/theme.js
+++ src/js/theme.js
@@ -2,12 +2,28 @@ const toggler = document.querySelector('.switch-theme-checkbox')
 const rootElement = document.querySelector('body')
 
 function changeTheme() {
+
     if (toggler.checked) {       
         rootElement.classList.add('dark-theme')
+        localStorage.setItem("theme", "dark");
+
     } else
     {
-    rootElement.classList.remove('dark-theme')}
+        rootElement.classList.remove('dark-theme')
+        localStorage.removeItem("theme");
+    }
 
 }
 
-toggler.addEventListener('change',changeTheme)
+function localTheme() {
+    const localStorageTheme = localStorage.getItem("theme");
+    
+    if (localStorageTheme === null) { toggler.checked = false;return};
+        toggler.checked = true;
+        rootElement.classList.add('dark-theme')
+}
+
+
+toggler.addEventListener('change', changeTheme)
+
+window.addEventListener('load',localTheme)
